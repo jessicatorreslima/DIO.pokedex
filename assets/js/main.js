ecd903058a04b92b5574fcf694719ea586136d31
Jéssica Torres <jtlimapro@gmail.com>
2023-04-15 21:45:59 -0300

    draft of pokemon modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,65 +4,138 @@ const maxRecords = 151; // Pokémon from Generation I
 const limit = 12;
 let offset = 0;
 
-function loadMoreItens(offset, limit) {
-  function convertPokemonToLi(pokemon) {
-    const li = document.createElement('li');
-    li.className = `pokemon ${pokemon.type}`;
+function convertPokemonToLi(pokemon) {
+  // Create the li element
+  const li = document.createElement('li');
+  li.className = `pokemon ${pokemon.type}`;
+  li.dataset.bsToggle = 'modal';
+  li.dataset.bsTarget = '#pokemon-modal';
 
-    const numberSpan = document.createElement('span');
-    numberSpan.className = 'number';
-    numberSpan.textContent = `#${pokemon.number}`;
-    li.appendChild(numberSpan);
+  // Add event listener to the li element
+  li.addEventListener('click', () => {
+    updatePokemonModal(pokemon)
+  });
 
-    const nameSpan = document.createElement('span');
-    nameSpan.className = 'name';
-    nameSpan.textContent = pokemon.name;
-    li.appendChild(nameSpan);
+  // Create and append the number span
+  const numberSpan = document.createElement('span');
+  numberSpan.className = 'number';
+  numberSpan.textContent = `#${pokemon.number}`;
+  li.appendChild(numberSpan);
 
-    const detailDiv = document.createElement('div');
-    detailDiv.className = 'detail';
-    li.appendChild(detailDiv);
+  // Create and append the name span
+  const nameSpan = document.createElement('span');
+  nameSpan.className = 'name';
+  nameSpan.textContent = pokemon.name;
+  li.appendChild(nameSpan);
 
-    const typesOl = document.createElement('ol');
-    typesOl.className = 'types';
-    detailDiv.appendChild(typesOl);
+  // Create and append the detail div
+  const detailDiv = document.createElement('div');
+  detailDiv.className = 'detail';
+  li.appendChild(detailDiv);
 
-    pokemon.types.forEach(type => {
-      const typeLi = document.createElement('li');
-      typeLi.className = `type ${type}`;
-      typeLi.textContent = type;
-      typesOl.appendChild(typeLi);
-    });
+  // Create and append the types ol
+  const typesOl = document.createElement('ol');
+  typesOl.className = 'types';
+  detailDiv.appendChild(typesOl);
 
-    const img = document.createElement('img');
-    img.src = pokemon.photo;
-    img.alt = pokemon.name;
-    img.className = 'pokemon-image';
-    img.dataset.pokemonNumber = pokemon.number;
-    detailDiv.appendChild(img);
+  // Create and append a type li for each type in the pokemon's types array
+  pokemon.types.forEach(type => {
+    const typeLi = document.createElement('li');
+    typeLi.className = `type ${type}`;
+    typeLi.textContent = type;
+    typesOl.appendChild(typeLi);
+  });
 
-    return li;
-  }
+  // Create and append the image element
+  const img = document.createElement('img');
+  img.src = pokemon.photo;
+  img.alt = pokemon.name;
+  img.className = 'pokemon-image';
+  detailDiv.appendChild(img);
 
-  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-    const liArray = pokemons.map(convertPokemonToLi);
-    pokemonList.append(...liArray);
-  });   
+  return li;
 }
 
-loadMoreItens(offset, limit);
+function loadMoreItems(offset, limit) {
+  pokeApi.fetchPokemons(offset, limit)
+    .then((pokemons) => {
+      // Convert each Pokemon object to an <li> element and add them to the list
+      const pokemonListItems = pokemons.map(pokemon => convertPokemonToLi(pokemon));
+      pokemonList.append(...pokemonListItems);
+    })
+    .catch((error) => {
+      // Log any errors that occur while fetching Pokemons
+      console.error(`Error fetching pokemons: ${error}`);
+    });
+}
+
+// Loads firsts pokémon
+loadMoreItems(offset, limit);
 
 btnLoadMore.addEventListener('click', () => {
+  // Increment the offset by the limit
   offset += limit;
 
-  const qtdRecordNextPage = offset + limit;
+  // Calculate the number of records on the next page
+  const nextRecordsCount = offset + limit;
 
-  if (qtdRecordNextPage >= maxRecords) {
-    const newLimit = maxRecords - offset;
-    loadMoreItens(offset, newLimit);
+  // Determine the new limit based on the number of records on the next page
+  const newLimit = nextRecordsCount >= maxRecords ? maxRecords - offset : limit;
 
+  // Load more items with the new offset and limit
+  loadMoreItems(offset, newLimit);
+
+  // If there are no more records to load, remove the "Load More" button
+  if (nextRecordsCount >= maxRecords) {
     btnLoadMore.parentElement.removeChild(btnLoadMore);
-  } else {
-    loadMoreItens(offset, limit);
   }
 });
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function updatePokemonModal(pokemon) {
+  // Get the modal title element and set its text content to the pokemon's name
+  const modalTitle = document.querySelector('#pokemon-modal-title');
+  modalTitle.textContent = capitalizeFirstLetter(pokemon.name);
+
+  // Get the modal image element and set its source and alt attributes to the pokemon's photo and name, respectively
+  const modalImage = document.querySelector('#pokemon-modal-image');
+  modalImage.src = pokemon.photo;
+  modalImage.alt = `Imagem do ${pokemon.name}`;
+
+  // Get the abilities list element and clear its HTML contents
+  const abilitiesList = document.querySelector('#pokemon-modal-abilities');
+  abilitiesList.innerHTML = '';
+
+  // Loop through the pokemon's abilities and add each one as a list item to the abilities list
+  pokemon.abilities.forEach(ability => {
+    const abilityItem = document.createElement('li');
+    abilityItem.textContent = capitalizeFirstLetter(ability.name);
+    abilitiesList.appendChild(abilityItem);
+  });
+
+  // Get the stats list element and clear its HTML contents
+  const statsList = document.querySelector('#pokemon-modal-stats');
+  statsList.innerHTML = '';
+
+  // Loop through the pokemon's stats and add each one as a list item to the stats list
+  pokemon.stats.forEach(statSlot => {
+    const statItem = document.createElement('li');
+    statItem.textContent = `${capitalizeFirstLetter(statSlot.name)}: ${statSlot.base_stat}`;
+    statsList.appendChild(statItem);
+  });
+
+  // Get the moves list element and clear its HTML contents
+  const movesList = document.querySelector('#pokemon-modal-moves');
+  movesList.innerHTML = '';
+
+  // Loop through the pokemon's moves and add each one as a list item to the moves list
+  pokemon.moves.forEach(move => {
+    const moveItem = document.createElement('li');
+    moveItem.textContent = capitalizeFirstLetter(move.name);
+    movesList.appendChild(moveItem);
+  });
+}
+
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,34 +1,56 @@
 const pokeApi = {};
 
 function convertPokeApiDetailPokemon(pokeDetail) {
-    const pokemon = new Pokemon();
-    pokemon.number = pokeDetail.id;
-    pokemon.name = pokeDetail.name;
+  const pokemon = new Pokemon();
 
-    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
-    const [type] = types; // get position 0 of array types and assign to variable 'type'
+  pokemon.number = pokeDetail.id;
+  pokemon.name = pokeDetail.name;
+  pokemon.types = getPokemonTypes(pokeDetail.types);
+  pokemon.type = pokemon.types[0];
+  pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+  pokemon.height = pokeDetail.height;
+  pokemon.weight = pokeDetail.weight;
+  pokemon.abilities = getPokemonAbilities(pokeDetail.abilities);
+  pokemon.stats = getPokemonStats(pokeDetail.stats);
+  pokemon.moves = getPokemonMoves(pokeDetail.moves);
 
-    pokemon.types = types;
-    pokemon.type = type;
-
-    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
-
-    return pokemon;
+  return pokemon;
 }
 
-pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url)
-        .then((response) => response.json())
-        .then(convertPokeApiDetailPokemon);
+function getPokemonTypes(types) {
+  return types.map(({ type }) => type.name);
+}
+
+function getPokemonAbilities(abilities) {
+  return abilities.map(({ ability, is_hidden, slot }) => ({
+    name: ability.name,
+    is_hidden,
+    slot,
+  }));
+}
+
+function getPokemonStats(stats) {
+  return stats.map(({ base_stat, stat }) => ({ base_stat, name: stat.name }));
+}
+
+function getPokemonMoves(moves) {
+  return moves.map(({ move }) => ({ name: move.name }));
+}
+
+
+pokeApi.fetchPokemonDetail = (pokemon) => {
+  return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertPokeApiDetailPokemon);
 };
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+pokeApi.fetchPokemons = (offset = 0, limit = 5) => {
+  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-    return fetch(url)
-        .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-        .then((detailRequests) => Promise.all(detailRequests))
-        .catch((error) => console.error(error));
+  return fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => pokemons.map(pokeApi.fetchPokemonDetail))
+    .then((detailRequests) => Promise.all(detailRequests))
+    .catch((error) => console.error(error));
 };
